04d0825901f2e5c10a2545ba9512a7cfd0a3a519
Andrew Harris <aharri@microsoft.com>
2019-09-04 21:46:24 -0400

    test file:// for xMsiPackage

--- DSC/ProvisionVictimPcDsc.ps1
+++ DSC/ProvisionVictimPcDsc.ps1
@@ -544,7 +544,7 @@ Configuration SetupVictimPc
 		xMsiPackage InstallAipClient
 		{
 			Ensure = 'Present'
-			Path = 'file://C:\LabTools\aip_ul_installer.msi'
+			Path = 'file://C:/LabTools/aip_ul_installer.msi'
 			ProductId = '3C393E78-A1A6-43E8-86C0-E9B22AB83143'
             Arguments = '/quiet'
             
@@ -564,8 +564,7 @@ Configuration SetupVictimPc
                 $tools = @(
                     ('https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20190512/mimikatz_trunk.zip', 'C:\Tools\Mimikatz.zip'),
                     ('https://github.com/PowerShellMafia/PowerSploit/archive/master.zip', 'C:\Tools\PowerSploit.zip'),
-                    ('https://github.com/gentilkiwi/kekeo/releases/download/2.2.0-20190407/kekeo.zip', 'C:\Tools\kekeo.zip'),
-                    ('')
+                    ('https://github.com/gentilkiwi/kekeo/releases/download/2.2.0-20190407/kekeo.zip', 'C:\Tools\kekeo.zip')
                 )
                 foreach ($tool in $tools){
                     Invoke-WebRequest -Uri $tool[0] -OutFile $tool[1]
