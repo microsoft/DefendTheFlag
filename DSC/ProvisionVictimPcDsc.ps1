493ed55435a67e4f143efb10851f6150928317f9
Andrew Harris <aharri@microsoft.com>
2019-10-09 01:37:14 -0400

    TLS update

--- DSC/ProvisionVictimPcDsc.ps1
+++ DSC/ProvisionVictimPcDsc.ps1
@@ -547,6 +547,7 @@ Configuration SetupVictimPc
         {
             DestinationPath = 'C:\LabTools\aip_ul_installer.msi'
             Uri = 'https://github.com/microsoft/DefendTheFlag/blob/v1.0/Downloads/AIP/Client/AzInfoProtection_UL_Preview_MSI_for_central_deployment.msi?raw=true'
+            DependsOn = '[Computer]JoinDomain'
         }
 		xMsiPackage InstallAipClient
 		{
