0bdc418eb6c921f292e30dc810483b4a86d2f4a2
Andrew Harris (AZURE SEC) <aharri@microsoft.com>
2019-10-18 14:21:15 -0400

    Bug fix

--- DSC/ProvisionVictimPcDsc.ps1
+++ DSC/ProvisionVictimPcDsc.ps1
@@ -522,20 +522,20 @@ Configuration SetupVictimPc
         #endregion
 
         #region AipClient
-        # xRemoteFile AipClient
-        # {
-        #     DestinationPath = 'C:\LabTools\aip_ul_installer.msi'
-        #     Uri = 'https://github.com/microsoft/DefendTheFlag/blob/v1.0/Downloads/AIP/Client/AzInfoProtection_UL_Preview_MSI_for_central_deployment.msi?raw=true'
-        #     DependsOn = '[Computer]JoinDomain'
-        # }
+        xRemoteFile AipClient
+        {
+            DestinationPath = 'C:\LabTools\aip_ul_installer.msi'
+            Uri = 'https://github.com/microsoft/DefendTheFlag/blob/v1.0/Downloads/AIP/Client/AzInfoProtection_UL_Preview_MSI_for_central_deployment.msi?raw=true'
+            DependsOn = '[Computer]JoinDomain'
+        }
         
 		MsiPackage InstallAipClient
 		{
             Ensure = 'Present'
-            Path = 'https://github.com/microsoft/DefendTheFlag/blob/v1.0/Downloads/AIP/Client/AzInfoProtection_UL_Preview_MSI_for_central_deployment.msi?raw=true'
+            Path = 'C:\LabTools\aip_ul_installer.msi'
             ProductId = '{B6328B23-18FD-4475-902E-C1971E318F8B}'
             Arguments = '/quiet'
-            DependsOn = '[Computer]JoinDomain'
+            DependsOn = '[xRemoteFile]AipClient'
         }
         #endregion
 
