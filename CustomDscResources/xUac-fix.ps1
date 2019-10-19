93eef8589132d099b32bda3331d13f40bbcff116
Andrew Harris (AZURE SEC) <aharri@microsoft.com>
2019-10-18 22:09:06 -0400

    Dsc Updates

---
+++ CustomDscResources/xUac-fix.ps1
@@ -0,0 +1,9 @@
+## should be committed back to xSystemSecurity
+Import-Module xDSCResourceDesigner
+
+$properties = @(
+    New-xDscResourceProperty -Name Setting -Type String -Attribute Key -ValidateSet "AlwaysNotify","NotifyChanges","NotifyChangesWithoutDimming","NeverNotify","NeverNotifyAndDisableAll"
+    New-xDscResourceProperty -Name Force -Type Boolean -Attribute Write -ValidateSet $true,$false
+)
+
+New-xDscResource -Name ah_xSystemSecurity -Property $properties -Path
