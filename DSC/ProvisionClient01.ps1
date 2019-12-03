4784f4f601527304dd4dc3b5e12e027c829f6346
Andrew Harris (AZURE SEC) <aharri@microsoft.com>
2019-12-03 00:07:53 -0500

    xMsiPackage -> xPackage

--- DSC/ProvisionClient01.ps1
+++ DSC/ProvisionClient01.ps1
@@ -382,9 +382,10 @@ Configuration SetupAipScannerCore
         #endregion
 
         #region AipClient
-		xMsiPackage InstallAipClient
+		xPackage InstallAipClient
 		{
             Ensure = 'Present'
+            Name = 'Microsoft Azure Information Protection'
             Path = "https://github.com/microsoft/DefendTheFlag/raw/$Branch/Downloads/AIP/Client/AzInfoProtection_UL_Preview_MSI_for_central_deployment.msi"
             ProductId = 'B6328B23-18FD-4475-902E-C1971E318F8B'
             DependsOn = @('[Computer]JoinDomain', '[Registry]SchUseStrongCrypto', '[Registry]SchUseStrongCrypto64')
